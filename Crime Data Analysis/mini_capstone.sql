53f3cd9c523a2c4fcfa44a44b25aa489b5af0666
PalakPastagiya <palak.jangbari.pj@gmail.com>
2024-08-19 21:36:54 -0600

    Update mini_capstone.sql

--- Crime Data Analysis/mini_capstone.sql
+++ Crime Data Analysis/mini_capstone.sql
@@ -1,5 +1,7 @@
-select * from crime_dataset;
+-- Data Exploration
 
+-- Distinct values of different columns of Crime dataset
+select * from crime_dataset;
 select distinct DR_NO from crime_dataset;
 select distinct AREA_NAME from crime_dataset;
 select distinct Crm_Cd from crime_dataset;
@@ -14,34 +16,32 @@ select distinct LAT from crime_dataset;
 select distinct LON from crime_dataset;
 select distinct Date_Rptd from crime_dataset;
 select distinct DATE_OCC from crime_dataset;
-
-
 select year(Date_Rptd) from crime_dataset;
 select distinct Vict_Age from crime_dataset; 
+
+-- Location based analysis
+
 select Premis_Desc from crime_dataset;
 select  year(DATE_OCC),crm_cd from crime_dataset;
 select count(Premis_Desc),Premis_Desc from crime_dataset
 group by Premis_Desc
 having count(Premis_Desc)>1;
 
-
 (select count(Premis_Desc),Premis_Desc from crime_dataset
 group by Premis_Desc
 having count(Premis_Desc)>1);
  
-
 select count(Premis_Desc),Premis_Desc from crime_dataset
 group by Premis_Desc
 having count(Premis_Desc)=1;
 
-
 select Date_Rptd,Crm_Cd_Desc, Status from crime_dataset;
-
 
 select status,Crm_Cd_Desc from crime_dataset 
 group by status,Crm_Cd_Desc;
 select * from crime_dataset;
 
+-- Crime status based analysis
 
 select distinct Status from crime_dataset;
 select Date_Rptd,Crm_Cd_Desc, Status from crime_dataset where status ='AO';
@@ -51,9 +51,10 @@ select Date_Rptd,Crm_Cd_Desc, Status from crime_dataset where status='JO';
 select count(Status) ,Status from crime_dataset 
 group by Status;
 
--- DR_NO,Date_Rptd,DATE_OCC,AREA_NAME,Crm_Cd,Crm_Cd_Desc,Vict_Age,Vict_Sex,Premis_Desc,Status,Location,LAT,LON
-select distinct crm_cd,Crm_Cd_Desc from crime_dataset;
 
+-- Victim demographics:
+
+select distinct crm_cd,Crm_Cd_Desc from crime_dataset;
 select Vict_Age from crime_dataset where Vict_Age=0;
 select distinct(year(DATE_OCC)) from crime_dataset;
 select count(*),monthname(DATE_OCC) from crime_dataset
@@ -65,9 +66,6 @@ group by  Crm_Cd,DATE_OCC order by c desc;
 select distinct Crm_Cd,count(*) as c, Date_Rptd from crime_dataset
 group by  Crm_Cd,Date_Rptd order by c desc;
 
-
-select * from crime_dataset;
-
 select count(*),dayname(Date_Rptd) from crime_dataset
 group by dayname(Date_Rptd);
 
@@ -75,11 +73,6 @@ select count(*) ,Vict_Age from crime_dataset
 group by Vict_Age order by Vict_Age;
 select count(*) , Vict_Age from crime_dataset
 group by Vict_Age order by Vict_Age;
-
-
-select * from crime_dataset where Vict_Age=0;
-
-
 
 select * from crime_dataset where Crm_Cd=330;
 
@@ -98,6 +91,9 @@ as day_delay,Date_Rptd from crime_dataset) a
 group by day_delay;
 select distinct (year(Date_Rptd)) from crime_dataset;
 select distinct (year(DATE_OCC)) from crime_dataset;
+
+-- Spatial analysis
+
 select count(*) as crime_count,LAT,LON,AREA_NAME from crime_dataset where LAT=LAT and LON=LON
 group by LAT,LON,AREA_NAME order by crime_count desc;
 
@@ -117,10 +113,8 @@ select count(*) as count ,Premis_Desc from crime_dataset group by Premis_Desc or
 
 select count( status),status from crime_dataset
 group by  status;
-
-
 select * from crime_dataset;
 select count(*) as No_of_crime,monthname(DATE_OCC) month ,year( DATE_OCC)as year from crime_dataset
- group by monthname(DATE_OCC) ,year( DATE_OCC) order by no_of_crime desc
+group by monthname(DATE_OCC) ,year( DATE_OCC) order by no_of_crime desc
 
  
